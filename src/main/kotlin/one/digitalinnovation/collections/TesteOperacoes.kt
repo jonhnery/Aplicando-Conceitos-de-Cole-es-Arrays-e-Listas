25fc30e4635de799e77ca31d4c9515711c122b2f
jonhnery <jonatahnery@gmail.com>
2022-07-17 07:45:36 -0300

    Operações com Array 2

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -7,11 +7,24 @@ fun main() {
     println(salario)
     }
     println("********************************")
-    println("Maior salário: ${salarios.max()}")
-    println("Menor salário: ${salarios.min()}")
+    println("Maior salário: ${salarios.maxOrNull()}")
+    println("Menor salário: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
 
     println("********************************")
-    val salariosfiltro = salarios.filter { it < 2500.0 }
+    val salariosfiltro = salarios.filter { it < 2500.0 } //filtrar valores
     salariosfiltro.forEach{ println(it) }
+
+    println("********************************")
+    println(salarios.count{ it in 2000.0..5000.0}) //contar valores
+
+    println("********************************")
+    println(salarios.find{ it == 2250.0}) //buscar valor
+    println(salarios.find{ it == 500.0}) //buscar valor
+
+    println("********************************")
+    println(salarios.any{ it == 1000.0}) //buscar qualquer valor resposta booleana
+    println(salarios.any{ it == 100.0}) //buscar qualquer valor resposta booleana
+
+
 }
