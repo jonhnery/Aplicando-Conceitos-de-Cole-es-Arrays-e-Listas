13d60bcf9ad53dc349cf468f06f0762daad02d34
jonhnery <jonatahnery@gmail.com>
2022-07-18 22:28:37 -0300

    SetOf

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = funcionario("João",2000.0)
-    val maria = funcionario("Maria",1500.0)
-    val pedro = funcionario("Pedro",5000.0)
+    val joao = funcionario("João",2000.0,"CLT")
+    val maria = funcionario("Maria",1500.0,"PJ")
+    val pedro = funcionario("Pedro",5000.0,"CLT")
 
     val funcionarios = listOf(joao,maria,pedro)
 
@@ -13,18 +13,12 @@ fun main() {
     println(funcionarios.find { it.nome == "Maria"})
 
     println("********************************")
-    funcionarios.sortedBy { it.salario }.forEach{ println(it) }
-}
+    funcionarios.sortedBy { it.salario }
+        .forEach{ println(it) }
 
-data class funcionario(
-    val nome:String,
-    val salario:Double
-) {
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salário: $salario
-            
-        """.trimIndent()
+    println("********************************")
+    funcionarios.groupBy { it.tipocontratacao }
+        .forEach{ println(it) }
 
 }
+
